ed9d3e137c588da8021dc8bf7577fbb4f2eb8d98
Dajokaem <jos562374@gmail.com>
2024-04-03 21:13:55 -0600

    Finalizado

--- src/arbol_b/Arbol_B.java
+++ src/arbol_b/Arbol_B.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arbol_b;
+
+import Recursos.Arbol;
+import java.util.Scanner;
 
 /**
  *
@@ -10,6 +9,39 @@ package arbol_b;
  */
 public class Arbol_B {
 
-   
-    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int op = 99, grado, valor;
+        System.out.println("Este arbol de tipo B solo puede aceptar valores, mas puedes indicar el grado");
+        System.out.println("Cual sera el grado de este arbol?");
+        grado = leer.nextInt();
+        Arbol arbol = new Arbol(grado);
+        do{
+            menu();
+            op = leer.nextInt();
+            switch(op){
+                case 1:
+                    System.out.println("Ok, Dame el valor entero");
+                    valor = leer.nextInt();
+                    System.out.println("Gracias, el valor ha sido insertado");
+                    arbol.insertar(valor);
+                    break;
+                case 2:
+                    System.out.println("Esta bien, ahora te lo muestro");
+                    arbol.showBTree();
+                    break;
+                case 0: 
+                    System.out.println("Esta bien, Adios");
+                    break;
+            }
+        }while(op != 0);
+    }
+    public static void menu(){
+        System.out.println("");
+        System.out.println("Que quieres hacer?");
+        System.out.println("1. Insertar");
+        System.out.println("2. Mostrar");
+        System.out.println("0. Salir");
+        System.out.println("");
+    }
 }
